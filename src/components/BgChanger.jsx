145090f886cf72a19658370f99910e1d0f615054
the-code-teller <officialgupta21@gmail.com>
2025-01-21 07:17:48 +0530

    BgChanger updated for generic button and responsive updates for phone

--- src/components/BgChanger.jsx
+++ src/components/BgChanger.jsx
@@ -24,13 +24,15 @@ const BgChanger = () => {
       className="h-screen flex items-end justify-center"
       style={{ backgroundColor: bgColor }}
     >
-      <div className="bg-white p-2 m-5 fixed rounded-2xl flex gap-2">
+      <div className="bg-white p-2 m-5 fixed rounded-2xl flex flex-wrap gap-2">
         {colorOptions.map((color) => (
           <Button
+            key={color}
             type="button"
             backgroundColor={color}
-            onClick={setBgColor}
-            value={color}
+            onClick={() => setBgColor(color)}
+            value={color.toUpperCase()}
+            className="rounded-2xl w-24 border text-gray-100"
           />
         ))}
       </div>
